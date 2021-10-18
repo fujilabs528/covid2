82afe92fd59de2e84507a708dddc46abe9e81b72
fujilabs528 <73245135+fujilabs528@users.noreply.github.com>
2021-10-18 13:44:30 -0700

    Add files via upload

---
+++ junk.js
@@ -0,0 +1,13 @@
+var mmm = new XMLHttpRequest();
+            mmm.onload = function() {
+                if (mmm.status === 200) {
+                    ResponseObject = JSON.parse(mmm.responseText)
+                }
+
+                var nnn = ``
+
+            }
+            mmm.open("GET",url, true)
+            mmm.send(null)
+
+var yyy= '<h1>suck my dick </h1>'
